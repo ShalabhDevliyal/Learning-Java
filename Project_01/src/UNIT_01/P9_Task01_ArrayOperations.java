83ca0656a01b23ce4dfd72f0a8fc695861541a45
ShalabhDevliyal <shalabhdevliyal@gmail.com>
2022-04-10 02:07:58 +0530

    Java Progress Update.

---
+++ Project_01/src/UNIT_01/P9_Task01_ArrayOperations.java
@@ -0,0 +1,55 @@
+package UNIT_01;
+
+/* Tasks given below:
+obj.sortAnArray(array);
+obj.findTheDuplicateElements(array);
+obj.findSecondLargestAndSecondSmallestElement(array);
+obj.leftRotationInAnArray(array); 
+obj.removeElementInArray(array);//(Optional)
+*/
+
+public class P9_Task01_ArrayOperations {
+
+public static void main(String[] args) {
+
+int[] array = { 1, 2, 3, 4, 5, 4, 6, 2, 3 };
+
+QuestionsOnArray obj = new QuestionsOnArray();
+obj.sortAnArray(array);
+obj.findTheDuplicateElements(array);
+obj.findSecondLargestAndSecondSmallestElement(array);
+obj.leftRotationInAnArray(array);
+obj.removeElementInArray(array); // (Optional)
+
+}
+
+}
+
+class QuestionsOnArray {
+
+void sortAnArray(int[] arr) {
+// write code here!
+
+}
+
+void findTheDuplicateElements(int[] arr) {
+// write code here!
+
+}
+
+void findSecondLargestAndSecondSmallestElement(int[] arr) {
+// write code here!
+
+}
+
+void leftRotationInAnArray(int[] arr) {
+// write code here!
+
+}
+
+void removeElementInArray(int[] arr) {
+// write code here!
+
+}
+
+}
