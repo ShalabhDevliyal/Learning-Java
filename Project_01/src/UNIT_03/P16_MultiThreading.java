6696264ed743791d382866f86a51c4c8ff8cb705
ShalabhDevliyal <shalabhdevliyal@gmail.com>
2022-05-21 12:12:53 +0530

    upload

--- Project_01/src/UNIT_03/P16_MultiThreading.java
+++ Project_01/src/UNIT_03/P16_MultiThreading.java
@@ -134,7 +134,7 @@ class T3 implements Runnable {
 	 
 	 public void run() {
 		 try {
-				Thread.sleep(5000);
+				Thread.sleep(10000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
