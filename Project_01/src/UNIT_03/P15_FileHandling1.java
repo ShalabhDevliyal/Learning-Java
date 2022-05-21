b482d7e118d35d86fdf4a938db6b9862d7224031
ShalabhDevliyal <shalabhdevliyal@gmail.com>
2022-05-21 10:31:40 +0530

    update

---
+++ Project_01/src/UNIT_03/P15_FileHandling1.java
@@ -0,0 +1,26 @@
+package UNIT_03;
+public class P15_FileHandling1 {
+
+	public static void main(String[] args) {
+		FileHandling obj=new FileHandling();
+	//	obj.takeInputFromAFileNotFromConsole();
+		obj.saveOutputIntoAFile();
+		obj.createALogFile();
+	}
+
+}
+class FileHandling
+{
+	static int a=10;
+	void takeInputFromAFileNotFromConsole() throws IOException
+	{
+		
+	}
+	void saveOutputIntoAFile()
+	{
+		
+	}
+	void createALogFile()
+	{
+		
+	}
--- Project_01/src/UNIT_03/P16_MultiThreading.java
+++ Project_01/src/UNIT_03/P16_MultiThreading.java
@@ -1,5 +1,32 @@
 package UNIT_03;
 
+/*
+ * Multiprocessing :-
+ *                 - parallel processing
+ *                 -Sequential processing
+ *                 -concurrent processing
+ *                 
+ * Multitasking :-
+ *             -Parallel multitasking
+ *             -Sequential multitasking
+ *             -Concurrent multitasking
+ *                 
+ * Program counter :- holds the address of the next instruction to be executed 
+ *                 by 1 processing unit.
+ *                 
+ *                 
+ * MultiThreading Task :- 
+ *                 *It is users job to find out the independent task of that process.
+ *                 *Your findings are T1,T2,T3,T4 Task of process T1 out of 8 total task.
+ *                 *Context switching between these task and the worst time 
+ *                  is not decided by the user instead JVM will decide this.
+ *                 *MultiThreading is applied when concurrent multitasking occurs.
+ *                 *It is users job to put these independent codes into their 
+ *                  respective threads.
+ *                 *User have to start these threads by T1.start,T2.start,T3.start,T4.start.
+ *     
+ *                 
+ */
 public class P16_MultiThreading {
 
 	public static void main(String[] args) {
