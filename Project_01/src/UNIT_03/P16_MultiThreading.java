e4f84dbc20f80eae2f7076e6efc376b221cbe45f
ShalabhDevliyal <shalabhdevliyal@gmail.com>
2022-05-21 12:12:39 +0530

    update

--- Project_01/src/UNIT_03/P16_MultiThreading.java
+++ Project_01/src/UNIT_03/P16_MultiThreading.java
@@ -133,11 +133,14 @@ class T2 implements Runnable {
 class T3 implements Runnable {
 	 
 	 public void run() {
+		 try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 System.out.println("HUE HUE T3");
 		 
-		 for(int i=0;i<10;i++)
-		 {
-			 System.out.println("HUE HUE T3");
-		 }
 		 
 	 }
 }
@@ -145,14 +148,12 @@ class T4 extends Thread {
 	 
 	 public void run() {
 		 
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
+		 for(int i=0;i<10;i++)
+		 {
 			 System.out.println("HUE HUE T4");
+		 }
+		
+			
 		 
 	 }
 }
