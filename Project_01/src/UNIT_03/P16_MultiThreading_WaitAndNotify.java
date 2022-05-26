e1b53f0502c53a235837355bb7716ee54dddb705
ShalabhDevliyal <shalabhdevliyal@gmail.com>
2022-05-26 13:47:27 +0530

    commit

--- Project_01/src/UNIT_03/P16_MultiThreading_WaitAndNotify.java
+++ Project_01/src/UNIT_03/P16_MultiThreading_WaitAndNotify.java
@@ -2,7 +2,6 @@ package UNIT_03;
 
 
 /* -
- * 
  * InterThread Communication:
    - Threads uses wait, notify() and notifyAll() methods from Object Clas
    - command + Shift + T -> Find object class and see its methods
